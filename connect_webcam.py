1b9371ef75c5509699d3756081f8716137deae6a
Júlía Arnardóttir <julia.arnardottir@gmail.com>
2019-05-31 19:14:04 -0700

    saving frames

--- connect_webcam.py
+++ connect_webcam.py
@@ -1,20 +1,32 @@
-import numpy as np
+# python connect_webcam.py
+
 import cv2
+print(cv2.__version__)
+vidcap = cv2.VideoCapture(0)
+success, image = vidcap.read()
+count = 0
+success = True
+while success:
+  # Capture frame-by-frame
+  ret, frame = vidcap.read()
 
-cap = cv2.VideoCapture(0)
+  # Our operations on the frame come here
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+  # Display the resulting frame
+  cv2.imshow('frame', gray)
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    print(cv2.waitKey(1))
+    break
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  if count == 500:
+    break
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+  success, image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
 
 # When everything done, release the capture
-cap.release()
+vidcap.release()
 cv2.destroyAllWindows()
