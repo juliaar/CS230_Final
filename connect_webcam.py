bf590543af501c50b23316b90635635378a62900
Júlía Arnardóttir <julia.arnardottir@gmail.com>
2019-06-02 12:02:45 -0700

    working

--- connect_webcam.py
+++ connect_webcam.py
@@ -1,32 +1,51 @@
 # python connect_webcam.py
 
 import cv2
-print(cv2.__version__)
+
+def getFrame(count):
+  # vidcap.set(cv2.CAP_PROP_POS_MSEC, sec*1000)
+  hasFrames, image = vidcap.read()
+  if hasFrames:
+    cv2.imwrite("frame" + str(count) + ".jpg", image)  # save frame as JPG file
+  return hasFrames
+
+
 vidcap = cv2.VideoCapture(0)
-success, image = vidcap.read()
+# automatically captures 30 fps (we want to show those but only save 3 fps)
+#print(vidcap.get(cv2.CAP_PROP_FPS))
+totcount = 0
 count = 0
-success = True
-while success:
-  # Capture frame-by-frame
-  ret, frame = vidcap.read()
 
-  # Our operations on the frame come here
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = vidcap.read()
 
-  # Display the resulting frame
-  cv2.imshow('frame', gray)
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    print(cv2.waitKey(1))
-    break
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-  if count == 500:
-    break
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
 
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
-  success, image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
+    if (totcount % 10) == 0:
+      count = count + 1
+      # Only want to store 5 images
+      if count == 6:
+        count = 1
+      # Save frame
+      getFrame(count)
+      print(count)
+
+    if totcount == 200:
+      break
+
+    totcount = totcount + 1
+
+    # Stop if q is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 
 # When everything done, release the capture
 vidcap.release()
 cv2.destroyAllWindows()
+
